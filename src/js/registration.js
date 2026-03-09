0232541f1be967f2a775a2bede92af83052bd070
Silelv <sil.elv716@gokstadakademiet.no>
2026-03-09 22:33:00 +0100

    added registration css file + fixed footer on registration site

---
+++ src/js/registration.js
@@ -0,0 +1,23 @@
+
+const buttonGroups = document.querySelectorAll(".form-card");
+
+buttonGroups.forEach(group => {
+  const buttons = group.querySelectorAll(".form-btn");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+      
+      if (button.classList.contains("active")) {
+        button.classList.remove("active");
+        return;
+      }
+
+      
+      buttons.forEach(btn => btn.classList.remove("active"));
+
+    
+      button.classList.add("active");
+    });
+  });
+});
