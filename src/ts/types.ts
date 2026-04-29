f4b6dd7994b6a0870cede49b480cd734c4d37050
vildehf <vildefristad@gmail.com>
2026-04-29 13:24:34 +0200

    smallfix

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,62 +1,59 @@
 export interface LoginResponse {
-    API_KEY: string
+  API_KEY: string;
 }
 export interface Users {
-    id: number
-    userName: string
-    password: string | number
-    email: string | number
-    description: string | null
-    dogs: Dog[]
-    created: string
-    updated: string
-    image?: string
+  id: number;
+  userName: string;
+  password: string | number;
+  email: string | number;
+  description: string | null;
+  dogs: Dog[];
+  created: string;
+  updated: string;
+  image?: string;
 }
 
 export interface Dog {
-    id: number
-    name: string
-    breed: string
-    gender: "Him" | "Her"
-    age: number
-    allergies: Allergy[]
-    created: string
-    updated: string
-    image?: string
+  id: number;
+  name: string;
+  breed: string;
+  gender: "Him" | "Her";
+  age: number;
+  allergies: Allergy[];
+  created: string;
+  updated: string;
+  image?: string;
 }
 
-export interface Allergy {
-    
-}
+export interface Allergy {}
 
-export interface petSitters {
-    id: number
-    name: string
-    image?: string
-    location: string
-    pricePerDay: number
-    rating: number
-    reviewCount: number
-    acceptsPuppies: boolean
-    acceptsLargeDogs: boolean
-    yearsOfExperience: number
-    experienceDescription: string | null
-    available: boolean
-    created: string
-    updated: string
+export interface PetSitters {
+  id: number;
+  name: string;
+  image?: string;
+  location: string;
+  pricePerDay: number;
+  rating: number;
+  reviewCount: number;
+  maxDogs: number;
+  acceptsPuppies: boolean;
+  acceptsLargeDogs: boolean;
+  yearsOfExperience: number;
+  experienceDescription: string | null;
+  available: boolean;
+  created: string;
+  updated: string;
 }
 
 export interface Bookings {
-    id: number
-    userId: number
-    userDogId: number
-    petSitterId: number
-    fromDate: string | null
-    toDate: number
-    status: string
-    message: string
-    created: string
-    updated: string
+  id: number;
+  userId: number;
+  userDogId: number;
+  petSitterId: number;
+  fromDate: string | null;
+  toDate: string | null;
+  status: string;
+  message: string;
+  created: string;
+  updated: string;
 }
-
-
