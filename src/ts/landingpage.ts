c9a2d4f5c86e944f5efe9adca778645d84d8a5d6
Sudbo <mats.sudbo@gmail.com>
2026-05-13 13:09:08 +0200

    added requested changes

--- src/ts/landingpage.ts
+++ src/ts/landingpage.ts
@@ -1,11 +1,22 @@
+/* Siden er laget av Mats Sudbø */
+import { login } from "./api";
+
 const loginDialog = document.getElementById("loginDialog") as HTMLDialogElement | null;
 const openLogin = document.getElementById("openLogin") as HTMLButtonElement | null;
 const closeLogin = document.getElementById("closeLogin") as HTMLButtonElement | null;
 
 const loginForm = document.getElementById("loginForm") as HTMLFormElement | null;
 const loginEmail = document.getElementById("loginEmail") as HTMLInputElement | null;
+const loginPassword = document.getElementById("loginPassword") as HTMLInputElement | null;
 
-if (loginDialog && openLogin && closeLogin && loginForm && loginEmail) {
+if (
+  loginDialog &&
+  openLogin &&
+  closeLogin &&
+  loginForm &&
+  loginEmail &&
+  loginPassword
+) {
   openLogin.addEventListener("click", () => {
     loginDialog.showModal();
   });
@@ -17,30 +28,17 @@ if (loginDialog && openLogin && closeLogin && loginForm && loginEmail) {
   loginForm.addEventListener("submit", async (event) => {
     event.preventDefault();
 
-    const email = loginEmail.value.trim();
-
     try {
-      const response = await fetch(`http://localhost:3000/api/users?email=${email}`);
-
-      if (!response.ok) {
-        throw new Error("Kunne ikke hente bruker fra API");
-      }
-
-      const users = await response.json();
-
-      const user = users[0];
-
-      if (!user) {
-        alert("Fant ingen bruker med denne e-posten");
-        return;
-      }
-
-      localStorage.setItem("potepassUser", JSON.stringify(user));
+      await login(
+        loginEmail.value.trim(),
+        loginPassword.value.trim()
+      );
 
       window.location.href = "/src/pages/profile/profile.html";
+
     } catch (error) {
       console.error(error);
-      alert("Noe gikk galt ved innlogging");
+      alert("Feil e-post eller passord");
     }
   });
 }
